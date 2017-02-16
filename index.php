245f02dd6b8ea2f3f2721d44022111f5a61cbe19
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-02-16 21:32:37 +0100

    Upload app

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8"/>
+    <title>Piekarnia</title>
+</head>
+
+<body>
+ <h1>Oreder online</h1>
+ <form action="order.php" method="post">
+     How much Doughnuts (0.99$/piece):
+     <input type="text" name="doughnuts" />
+        <br><br>
+     How much Cakes (1.49$/piece):
+     <input type="text" name="cakes"/>
+        <br><br>
+     <input type="submit" value="Send order">
+ </form>
+
+</body>
+</html>
